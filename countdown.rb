1f9519241d72ad7b0cce994079404c50ef531c0c
Levik Naparstek <levik010@gmail.com>
2019-02-06 21:04:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown (x)
   x == 10
-  while x > 1
+  while x > 0
   puts "#{x} SECOND(S)!"
   x -=1 
   
@@ -13,7 +13,7 @@ end
 
 def countdown_with_sleep (x)
    x == 10
-  while x > 1
+  while x > 0
   puts "#{x} SECOND(S)!"
   x -=1 
   
