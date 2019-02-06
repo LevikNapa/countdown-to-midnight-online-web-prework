50bcb8064448bc1a3d22b4d48e80dcaeec2e35b2
Levik Naparstek <levik010@gmail.com>
2019-02-06 20:54:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown (x)
+  x == 10
+  while x > 0
+  x -=1 
+  puts "#{x} SECOND(s)!"
+end
+"HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(x)
+    x == 10
+  while x > 0
+  x -=1 
+  puts "#{x} SECOND(s)!"
+  sleep 1
+end
+"HAPPY NEW YEAR!"
 end
